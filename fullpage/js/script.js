e502780addc8c3303d8dd067985aca6e6745502d
ryougaogahara <157794652+ryougaogahara@users.noreply.github.com>
2025-01-09 15:12:28 +0900

    Add files via upload

---
+++ fullpage/js/script.js
@@ -0,0 +1,17 @@
+$('#fullpage').fullpage( {
+    autoScroll: true,
+    navigation: true,
+    anchors: [
+        'firstPage',
+        'secondPage',
+        'thirdPage',
+        'fourthPage',
+    ],
+    sectionsColor: [
+        '#ff5f44',
+        '#0798ec',
+        '#fc6c7c',
+        '#fec291',
+    ],
+    responsiveWidth: 900,
+});
